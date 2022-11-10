20334643883ae726a7de58c8ecd412937cce2f6e
Redux234 <lorenzo.gaetano2002@gmail.com>
2022-11-10 15:06:37 +0100

    Prompt base e calcolo prezzo

---
+++ script.js
@@ -0,0 +1,24 @@
+let Chilometri = prompt ("Inserire I Chilometri Da Percorrere");
+let Eta = prompt ("Inserire L' Età Del Passeggero");
+
+console.log(
+    `
+    -------------Informazioni-----------------
+    Chilometri: ${Chilometri}
+    Età: ${Eta}
+    `
+)
+
+NumeroChilometri = parseInt(Chilometri);
+PrezzoPienoBiglietto = (Chilometri * 0.21)
+
+console.log(
+    `
+    -------------Prezzo Biglietto-------------
+    Prezzo: ${PrezzoPienoBiglietto}
+    ` 
+)
+
+NumeroEta = parseInt(Eta)
+
+
